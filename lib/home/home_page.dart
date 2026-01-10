97337a39fc965871066ded504f7f9ed5dd7163a3
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2026-01-10 21:33:21 +0500

    feat(home): implement Home Page UI and components

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:fitness_tracker_application/appearance/appearance.dart';
+import 'package:fitness_tracker_application/home/home.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,18 +7,77 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const HomeView();
+    final color = context.color;
+    const avatarUrl =
+        'https://img.goodfon.ru/wallpaper/big/3/f4/poza-kostium-kianu-rivz-keanu-reeves-john-wick-dzhon-uik-por.webp';
+    const userName = 'John';
+    return Scaffold(
+      backgroundColor: color.backgroundPrimary,
+      appBar: const _HomeHeader(
+        userName: userName,
+        avatarUrl: avatarUrl,
+      ),
+      body: const _HomeBody(),
+    );
   }
 }
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+class _HomeHeader extends StatelessWidget implements PreferredSizeWidget {
+  const _HomeHeader({
+    this.userName,
+    this.avatarUrl,
+  });
+
+  final String? avatarUrl;
+  final String? userName;
+
+  static const double _headerHeight = 100;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home'),
+    final color = context.color;
+    final spacing = context.spacing;
+    return AppBar(
+      titleSpacing: spacing.sm,
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      backgroundColor: color.backgroundPrimary,
+      toolbarHeight: _headerHeight,
+      title: Row(
+        spacing: spacing.sm,
+        children: [
+          UserAvatar(url: avatarUrl),
+          Expanded(
+            child: UserGreeting(
+              name: userName,
+            ),
+          ),
+          const SearchButton(),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(_headerHeight);
+}
+
+class _HomeBody extends StatelessWidget {
+  const _HomeBody();
+
+  @override
+  Widget build(BuildContext context) {
+    final spacing = context.spacing;
+    return Padding(
+      padding: EdgeInsets.all(spacing.sm),
+      child: SingleChildScrollView(
+        child: Column(
+          spacing: spacing.sm,
+          children: const [
+            DailyChallengeCard(),
+            WeeklyListComponent(),
+          ],
+        ),
       ),
     );
   }
