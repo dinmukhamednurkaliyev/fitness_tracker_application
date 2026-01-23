24ee2c33579a2294b0492fa63ebad611201d84ce
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2026-01-23 20:37:07 +0500

    refactor(application): persist GoRouter instance using StatefulWidget

--- lib/application.dart
+++ lib/application.dart
@@ -1,18 +1,25 @@
 import 'package:fitness_tracker_application/appearance/appearance.dart';
 import 'package:fitness_tracker_application/navigation/navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class Application extends StatelessWidget {
+class Application extends StatefulWidget {
   const Application({super.key});
+
+  @override
+  State<Application> createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+  late final GoRouter _router = buildRouter();
 
   @override
   Widget build(BuildContext context) {
     final theme = buildTheme();
-    final router = buildRouter();
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: theme,
-      routerConfig: router,
+      routerConfig: _router,
     );
   }
 }
